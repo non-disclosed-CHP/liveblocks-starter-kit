d7d83428e33eea80595f2078544d66df0262fabd
non-disclosed-CHP <popsicletomato@gmail.com>
2024-12-12 00:57:52 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import YourComponent from '../components/YourComponent';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Your Liveblocks Project!</h1>
+      <YourComponent />
+    </div>
+  );
+}
